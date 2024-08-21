35fb8381634dd5eb90662b5f0374e579a097a3ef
Mahadi9990 <mdshafayat9999@gmail.com>
2024-08-21 09:45:28 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import { Button } from 'flowbite-react'
+
+export default function App() {
+  return (
+    <Button gradientDuoTone='purpleToPink' className='text-red-600 font-bold'>App</Button>
+  )
+}
